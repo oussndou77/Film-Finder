d849840d02c91c5456a01f5674de77b18f1519e6
oussndou77 <148482151+oussndou77@users.noreply.github.com>
2024-05-15 05:00:00 +0200

    Update main.js

--- main.js
+++ main.js
@@ -20,42 +20,24 @@ const getGenres = async () => {
   }
 }
 
-const getTotalPages = async (selectedGenre) => {
-    const discoverMovieEndpoint = '/discover/movie';
-    const requestParams = `?api_key=${tmdbKey}&with_genres=${selectedGenre}`;
-    const urlToFetch = `${tmdbBaseUrl}${discoverMovieEndpoint}${requestParams}`;
-
-    try {
-        const response = await fetch(urlToFetch);
-        if (response.ok) {
-            const jsonResponse = await response.json();
-            return jsonResponse.total_pages;
-        }
-    } catch (error) {
-        console.log(error);
-    }
-};
 
 const getMovies = async () => {
-    const selectedGenre = getSelectedGenre();
-    const totalPages = await getTotalPages(selectedGenre);
-    const randomPage = Math.floor(Math.random() * totalPages) + 1;
-    const discoverMovieEndpoint = '/discover/movie';
-    const requestParams = `?api_key=${tmdbKey}&with_genres=${selectedGenre}&page=${randomPage}`;
-    const urlToFetch = `${tmdbBaseUrl}${discoverMovieEndpoint}${requestParams}`;
+  const selectedGenre = getSelectedGenre();
+  const discoverMovieEndpoint = '/discover/movie';
+  const requestParams = `?api_key=${tmdbKey}&with_genres=${selectedGenre}`;
+  const urlToFetch = `${tmdbBaseUrl}${discoverMovieEndpoint}${requestParams}`;
 
-    try {
-        const response = await fetch(urlToFetch);
-        if (response.ok) {
-            const jsonResponse = await response.json();
-            const movies = jsonResponse.results;
-            return movies;
-        }
-    } catch (error) {
-        console.log(error);
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      const movies = jsonResponse.results;
+      return movies;
     }
+  } catch (error) {
+    console.log(error);
+  }
 };
-  
 
 const getMovieInfo = async (movie) => {
   const movieId = movie.id;
